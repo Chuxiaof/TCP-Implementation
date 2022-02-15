f141f5fce2e26525e33aa3d21799cecf2d3bd6d1
fimon-mbp <fimonlu@gmail.com>
2022-02-15 17:32:52 -0600

    finish multitimer

--- src/libchitcp/multitimer.c
+++ src/libchitcp/multitimer.c
@@ -45,23 +45,35 @@
 #include "chitcp/multitimer.h"
 #include "chitcp/log.h"
 
-static int timespec_cmp(struct timespec *x, struct timespec *y) {
-    if(x->tv_sec < y->tv_sec) {
+struct worker_args
+{
+    multi_timer_t *mt;
+};
+
+void *multiple_timer_machine(void *args);
+
+static int timespec_cmp(struct timespec *x, struct timespec *y)
+{
+    if (x->tv_sec < y->tv_sec)
+    {
         return -1;
-    } else if(x->tv_sec > y->tv_sec){
+    }
+    else if (x->tv_sec > y->tv_sec)
+    {
         return 1;
     }
     return x->tv_nsec - y->tv_nsec;
 }
 
 /* See multitimer.h */
-void timespec_add(struct timespec *expire_time, uint64_t timeout) {
-    long secs = timeout/(long)1e9;
+void timespec_add(struct timespec *expire_time, uint64_t timeout)
+{
+    long secs = timeout / (long)1e9;
     long nsec = timeout % (long)1e9;
     clock_gettime(CLOCK_REALTIME, expire_time);
     long temp = expire_time->tv_nsec + nsec;
-    expire_time->tv_sec += secs + temp/(long)1e9;
-    expire_time->tv_nsec = temp%(long)1e9;
+    expire_time->tv_sec += secs + temp / (long)1e9;
+    expire_time->tv_nsec = temp % (long)1e9;
 }
 
 /* See multitimer.h */
@@ -115,13 +127,30 @@ int mt_init(multi_timer_t *mt, uint16_t num_timers)
     pthread_mutex_init(&mt->lock, NULL);
     pthread_cond_init(&mt->condvar, NULL);
 
+    /* create multi-timer thread */
+    pthread_t multiple_timer_thread;
+    struct worker_args *wa = calloc(1, sizeof(struct worker_args));
+    wa->mt = mt;
+    if (pthread_create(&multiple_timer_thread, NULL, multiple_timer_machine, wa) != 0)
+    {
+        chilog(ERROR, "Could not create a multitimer thread");
+        free(wa);
+        pthread_exit(NULL);
+        return CHITCP_ETHREAD;
+    }
     return CHITCP_OK;
 }
 
 /* See multitimer.h */
 int mt_free(multi_timer_t *mt)
 {
-    free(mt->all_timers);
+    if (mt)
+    {
+        pthread_cond_destroy(&mt->condvar);
+        pthread_mutex_destroy(&mt->lock);
+        free(mt->all_timers);
+    }
+    free(mt);
     return CHITCP_OK;
 }
 
@@ -139,24 +168,39 @@ int mt_get_timer_by_id(multi_timer_t *mt, uint16_t id, single_timer_t **timer)
 /* See multitimer.h */
 int mt_set_timer(multi_timer_t *mt, uint16_t id, uint64_t timeout, mt_callback_func callback, void *callback_args)
 {
-    single_timer_t * timer;
-    if (mt_get_timer_by_id(mt, id, &timer) == CHITCP_EINVAL || timer->active) {
+    single_timer_t *timer;
+    if (mt_get_timer_by_id(mt, id, &timer) == CHITCP_EINVAL || timer->active)
+    {
         return CHITCP_EINVAL;
     }
 
+    pthread_mutex_lock(&mt->lock);
     timer->active = true;
     timer->callback = callback;
     timer->callback_args = callback_args;
     timespec_add(&timer->expire_time, timeout);
     LL_INSERT_INORDER(mt->active_timers, timer, timespec_cmp);
-    
+    pthread_cond_signal(&mt->condvar);
+    pthread_mutex_unlock(&mt->lock);
+
     return CHITCP_OK;
 }
 
 /* See multitimer.h */
 int mt_cancel_timer(multi_timer_t *mt, uint16_t id)
 {
-    /* Your code here */
+    single_timer_t *timer;
+    if (mt_get_timer_by_id(mt, id, &timer) == CHITCP_EINVAL || !timer->active)
+    {
+        return CHITCP_EINVAL;
+    }
+
+    pthread_mutex_lock(&mt->lock);
+    timer->active = false;
+    LL_DELETE(mt->active_timers, timer);
+    timer->next = NULL;
+    pthread_cond_signal(&mt->condvar);
+    pthread_mutex_unlock(&mt->lock);
 
     return CHITCP_OK;
 }
@@ -165,7 +209,15 @@ int mt_cancel_timer(multi_timer_t *mt, uint16_t id)
 int mt_set_timer_name(multi_timer_t *mt, uint16_t id, const char *name)
 {
     /* Your code here */
-
+    single_timer_t *timer;
+    if (mt_get_timer_by_id(mt, id, &timer) == CHITCP_EINVAL || !timer->active)
+    {
+        return CHITCP_EINVAL;
+    }
+    
+    pthread_mutex_lock(&mt->lock);
+    strncpy(timer->name, name, strlen(name));
+    pthread_mutex_unlock(&mt->lock);
     return CHITCP_OK;
 }
 
@@ -187,8 +239,10 @@ int mt_chilog_single_timer(loglevel_t level, single_timer_t *timer)
         /* Compute the appropriate value for "diff" here; it should contain
          * the time remaining until the timer times out.
          * Note: The timespec_subtract function can come in handy here*/
-        diff.tv_sec = 0;
-        diff.tv_nsec = 0;
+        struct timespec *res;
+        timespec_subtract(res, &timer->expire_time, &now);
+        diff.tv_sec = res->tv_sec;
+        diff.tv_nsec = res->tv_nsec;
         chilog(level, "%i %s %lis %lins", timer->id, timer->name, diff.tv_sec, diff.tv_nsec);
     }
     else
@@ -200,7 +254,45 @@ int mt_chilog_single_timer(loglevel_t level, single_timer_t *timer)
 /* See multitimer.h */
 int mt_chilog(loglevel_t level, multi_timer_t *mt, bool active_only)
 {
-    /* Your code here */
-
+    if (active_only) {
+        pthread_mutex_lock(&mt->lock);
+        single_timer_t *el;
+        LL_FOREACH(mt->active_timers, el) {
+            mt_chilog_single_timer(level, el);
+        }
+        pthread_mutex_unlock(&mt->lock);
+    } else {
+        for (int i = 0; i < mt->timer_num; i++) {
+            mt_chilog_single_timer(level, &mt->all_timers[i]);
+        }
+    }
     return CHITCP_OK;
+}
+
+void *multiple_timer_machine(void *args)
+{
+    struct worker_args *wa = (struct worker_args *)args;
+    multi_timer_t *mt = wa->mt;
+
+    while (true)
+    {
+        pthread_mutex_lock(&mt->lock);
+        if (!mt->active_timers)
+        {
+            pthread_cond_wait(&mt->condvar, &mt->lock);
+        }
+        else
+        {
+            single_timer_t *first_timer = mt->active_timers;
+            int rv = pthread_cond_timedwait(&mt->condvar, &mt->lock, &first_timer->expire_time);
+            if (rv == ETIMEDOUT)
+            {
+                first_timer->callback(mt, first_timer, first_timer->callback_args);
+                first_timer->active = false;
+                LL_DELETE(mt->active_timers, first_timer);
+                first_timer->next = NULL;
+            }
+        }
+        pthread_mutex_unlock(&mt->lock);
+    }
 }
